a8ce688b940a853ebcb6142179329201d2ec8f60
jayden-fn <160244860@student.rnngroup.ac.uk>
2026-01-08 09:09:40 +0000

    Delete take2 directory

--- take2/rolsa technologies/dcrm/website/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('calculate/', views.calculate, name='calculate'),
-    path('booking/', views.booking, name='booking'),
-]
-
